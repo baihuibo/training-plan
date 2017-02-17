4c3e1030fa4452637e4b8f4e742b44ab3cbc3d6f
baihuibo <baihuibo@hotmail.com>
2017-02-17 11:55:08 +0800

    类型练习题

---
+++ javascript 基础/练习题.js
@@ -0,0 +1,14 @@
+//Created by baihuibo on 2017/2/17.
+function getTypes() {
+    /**
+     * 实现返回所有参数类型
+     * window 和 document 视为 object
+     */
+
+}
+
+var types = getTypes('javascript', false, ['window'], window, true, 1992, document, window, typeof null, null, /testReg/);
+
+var equal = types === ['string', 'boolean', 'array', 'object', 'boolean', 'number', 'object', 'object', 'string', 'object', 'regexp'];
+
+alert('equal : ' + equal);
